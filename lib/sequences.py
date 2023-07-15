9777b560d171aca7485299d0fbc5108ef8c1b941
Dennis Demir <dennisjdemir@gmail.com>
2023-07-15 16:04:09 -0400

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    sequence = []
+    a,b = 0,1
+
+    while len(sequence) < length:
+        sequence.append(a)
+        a,b = b, a+b
+    
+    print(sequence)
     pass
